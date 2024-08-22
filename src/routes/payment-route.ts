bf572d2ded751f87cec603716926a93cee51d0fa
Manas Kumar Jena <109361353+Manaskumar2@users.noreply.github.com>
2024-08-22 16:26:54 +0530

    rate limit use in login

--- src/routes/payment-route.ts
+++ src/routes/payment-route.ts
@@ -2,10 +2,11 @@ import { Router } from 'express';
 const router = Router();
 
 import { authenticateJWT } from '../middlewares/auth.js';
+import { apiLimiter } from '../middlewares/rate-limits.js';
 import { createPaymentHandler, getPaymentHistoryHandler } from '../controllers/payment-cntrl.js';
 
 
 router.post('/pay',authenticateJWT,createPaymentHandler)
-router.get('/history',authenticateJWT,getPaymentHistoryHandler)
+router.get('/history',apiLimiter,authenticateJWT,getPaymentHistoryHandler)
 
 export default router;
--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -8,11 +8,13 @@ import {
     getAllUsersHandler 
 } from '../controllers/user-cntrl.js';
 import { authenticateJWT } from '../middlewares/auth.js';
+import { apiLimiter } from '../middlewares/rate-limits.js';
 
 
-router.post('/login', loginHandler);
-router.post('/register', createUserHandler);
+
+router.post('/login',apiLimiter, loginHandler);
 router.get('/', authenticateJWT, getAllUsersHandler); 
+router.post('/register',apiLimiter, createUserHandler);
 router.put('/update', authenticateJWT, updateUserHandler); 
 router.get('/:id', authenticateJWT, getUserByIdHandler); 
 
